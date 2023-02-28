0c4052679cb10bb1579238abaf17eafd922874e3
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-02-28 11:58:37 +0300

    Code Challenge Complete

---
+++ bin/run.rb
@@ -0,0 +1,21 @@
+require_relative '../config/environment'
+
+# product1 = Product.new("Soda", "Ksh 180")
+# product2 = Product.new("Flour", "Ksh 150")
+# product3 = Product.new("Extension", "Ksh 1000")
+
+# puts product1.name
+# puts product2.name
+# puts product3.name
+
+------------------------------------------------------
+##User instances
+user1 = User.new("Arthur")
+user2 = User.new("Michael Angelo")
+user3 = User.new("Leonardo Di Caprio")
+
+puts user1.name
+puts user2.name
+puts user3.name
+
+
